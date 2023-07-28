e21b99df41ac32564e31d52fd5e6a03298ff9103
eeheg <sjh8924@naver.com>
2023-07-28 16:59:39 +0900

    refactor(Problem1): 예외처리 추가 및 불필요한 동작 개선

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,7 @@ package onboarding;
 import java.util.*;
 
 public class Problem1 {
-
-    //책을 펼쳐서 나오는 페이지 번호 생성 메서드
+    //페이지 번호 무작위 생성 메서드
     static void createPageNum(List<Integer> user) {
         int pageNum = (int) (Math.random() * 396) + 3;
 
@@ -15,14 +14,13 @@ public class Problem1 {
             user.set(1, pageNum);
             user.set(0, user.get(1) - 1);
         }
-        System.out.print(user);
     }
 
     //페이지 번호의 각 자릿수 추출 매서드
     static List<Integer> getDigitOfPage(int pageNum) {
         List<Integer> digits = new ArrayList<>(Arrays.asList(1, 2, 3));
         digits.set(2, pageNum % 10);  //일의 자리수
-        digits.set(1, ((pageNum - digits.get(2)) / 10) % 10);  //십의 자리수
+        digits.set(1, ((int)(pageNum / 10) % 10));  //십의 자리수
         digits.set(0, (int) (pageNum / 100));  //백의 자리수
         return digits;
 
@@ -39,13 +37,15 @@ public class Problem1 {
 
     //각 자리수 곱 메서드
     static int getDigitMult(List<Integer> digits) {
-
         int mult = 1;
-        if (digits.get(0) == 0) {
+        if (digits.get(0) == 0) {  //100미만일 때
             for (int i = 1; i < 3; i++) {
-                mult *= digits.get(i);
+                if (digits.get(1) == 0) {  //한자리 수 일 때
+                    mult = digits.get(2);
+                }
+                mult *= digits.get(i);  //두자리 수 일 때
             }
-        } else if (digits.get(0) > 0) {
+        } else if (digits.get(0) > 0) {  //세자리 수 일 때
             for (int i = 0; i < 3; i++) {
                 mult *= digits.get(i);
             }
@@ -55,40 +55,79 @@ public class Problem1 {
 
     //최대값 메서드
     static int getMaxScore(List<Integer> user) {
+        List<Integer> digitsOfLeftPage = getDigitOfPage(user.get(0));
+        List<Integer> digitsOfRightPage = getDigitOfPage(user.get(1));
         List<Integer> Num = new ArrayList<>();
-        Num.add(getDigitSum(getDigitOfPage(user.get(0))));
-        Num.add(getDigitSum(getDigitOfPage(user.get(1))));
-        Num.add(getDigitMult(getDigitOfPage(user.get(0))));
-        Num.add(getDigitMult(getDigitOfPage(user.get(1))));
-        return Collections.max(Num);
+        Num.add(getDigitSum(digitsOfLeftPage));  //왼쪽 페이지 자리수 합
+        Num.add(getDigitMult(digitsOfLeftPage)); //왼쪽 페이지 자리수 곱
+        Num.add(getDigitSum(digitsOfRightPage)); //오른쪽 페이지 자리수 합
+        Num.add(getDigitMult(digitsOfRightPage)); //오른쪽 페이지 자리수 곱
+        return Collections.max(Num);  //최댓값 반환
     }
 
-    //solution: getMaxScore값 비교하여 승패 return
-    static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
-            return -1;
-        } else if (getMaxScore(pobi) > getMaxScore(crong)) {
-            return 1;
-        } else if (getMaxScore(pobi) < getMaxScore(crong)) {
-            return 2;
-        } else if (getMaxScore(pobi) == getMaxScore(crong)) {
-            return 0;
-        } else {
+    //try-catch로 예외처리 및 승패 반환
+    static int getBinaryResult (List<Integer> pobi, List<Integer> crong) {
+        try {
+            if (!isValidPageNumber(pobi)) {
+                throw new IllegalArgumentException("유효하지 않은 페이지번호가 입력되었습니다.");
+            }
+            if (!isValidPageNumber(crong)) {
+                throw new IllegalArgumentException("유효하지 않은 페이지번호가 입력되었습니다.");
+            }
+
+            int pobiMax = getMaxScore(pobi);
+            int crongMax = getMaxScore(crong);
+
+            if (pobiMax > crongMax) {
+                return 1;
+            } else if (pobiMax < crongMax) {
+                return 2;
+            } else if (pobiMax == crongMax) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } catch (IllegalArgumentException e) {
             return -1;
         }
     }
 
+    //예외처리 : 유효한 페이지번호인지 확인하는 메서드
+    static boolean isValidPageNumber(List<Integer> user) {
+        List<Integer> invalidPageNumber = new ArrayList<>(Arrays.asList(1,2,399,400));
+        for (Integer pageNumber : user) {
+            // 페이지 번호는 음수가 될 수 없다.
+            if (pageNumber < 0) {
+                return false;
+            }
+            //페이지 번호는 [1,2,399,400]가 될 수 없다.
+            if (invalidPageNumber.contains(pageNumber)) {
+                return false;
+            }
+        }
+        // 오른쪽페이지 번호는 왼쪽페이지 번호에 1을 더한 값 이다.
+        if (user.get(0)+1 != user.get(1)) {
+            return false;
+        }
+        return true;
+    }
+
+    //solution: getMaxScore값 비교하여 승패 return
+    static int solution(List<Integer> pobi, List<Integer> crong) {
+        System.out.println("pobi: " + pobi);
+        System.out.println("crong: " + crong);
+        int answer = getBinaryResult (pobi, crong);
+        System.out.println("result: " + answer);
+        return answer;
+    }
 
     public static void main(String[] args) {
         //각 user의 길이2 배열 생성
         List<Integer> pobi = new ArrayList<Integer>(Arrays.asList(1, 2));
         List<Integer> crong = new ArrayList<Integer>(Arrays.asList(1, 2));
-
         //user가 책을 펼침
         createPageNum(pobi);
         createPageNum(crong);
-
-        //solution: 승패 출력
-        System.out.print(solution(pobi, crong));
+        solution(pobi, crong);
     }
 }
