8217d197679425d3c7edddfeda14e6c99fab94f6
eeheg <sjh8924@naver.com>
2023-07-29 19:24:46 +0900

    refactor(Problem5): 가독성을 유지한 중복코드개선 및 예외처리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,43 +1,33 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
-    // 출금하는 금액: 1 ~ 1,000,000 자연수 무작위 생성 메서드
-    static int withdrawMoney () {
-        int money = (int)(Math.random() * 999999)+1;
-        return money;
-    }
 
     // money의 각 자리수 추출 메서드
     static List<Integer> getMoneyDigits (int money) {
+        List<Integer> moneyDigits = new ArrayList<>(); //금액의 자릿수
+        moneyDigits.add((int)(money/Math.pow(10,6)));
+        for(int i=5; i>=0; i--) {
+            moneyDigits.add((int)(money/Math.pow(10,i))%10);
+        }
+        // moneyDigits.add((int)(money / 1000000));
+        // moneyDigits.add((money / 100000) % 10);
+        // moneyDigits.add((money / 10000) % 10);
+        // moneyDigits.add((money / 1000) % 10);
+        // moneyDigits.add((money / 100) % 10);
+        // moneyDigits.add((money / 10) % 10);
+        // moneyDigits.add((money % 10));
 
-        List<Integer> moneyDigits = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0)); //금액의 자릿수
-
-        moneyDigits.set(0, (int) (money / 1000000));
-        moneyDigits.set(1, (int) (money / 100000) % 10);
-        moneyDigits.set(2, (int) (money / 10000) % 10);
-        moneyDigits.set(3, (int) (money / 1000) % 10);
-        moneyDigits.set(4, (int) (money / 100) % 10);
-        moneyDigits.set(5, (int) (money / 10) % 10);
-        moneyDigits.set(6, (int) (money % 10));
-
-        System.out.println(money + " 원");
-        System.out.println(moneyDigits);
         return moneyDigits;
-
     }
 
     // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 개수
     static List<Integer> getMoneyNum (int money) {
-
         List<Integer> moneyDigits = getMoneyDigits(money); //money의 각 자리수 배열
         List<Integer> numOfMoney = new ArrayList<>(Arrays.asList(0,0,0,0,0,0,0,0,0)); //각 화폐개수 배열 생성
 
         int tenThousand = moneyDigits.get(0)*100 + moneyDigits.get(1)*10 + moneyDigits.get(2); //만원단위수
-
         numOfMoney.set(0,(int)(tenThousand/5)); //5만 원
         numOfMoney.set(1, tenThousand%5); //1만 원
         numOfMoney.set(2, (int)(moneyDigits.get(3)/5)); //5천 원
@@ -48,19 +38,33 @@ public class Problem5 {
         numOfMoney.set(7, moneyDigits.get(5)%5); //십 원
         numOfMoney.set(8, moneyDigits.get(6)); //일 원
 
-        System.out.println(numOfMoney);
         return numOfMoney;
     }
 
+    static boolean isValidNumber (int money) {
+        if (money < 1 || money > 1000000) {
+            return false;
+        }
+        return true;
+    }
+
     public static List<Integer> solution(int money) {
-
-        List<Integer> answer = getMoneyNum(money);
-        return answer;
-
+        try {
+            if (!isValidNumber(money)) {
+                throw new IllegalArgumentException("유효하지 않은 값이 입력되었습니다.");
+            }
+            List<Integer> answer = getMoneyNum(money);
+            System.out.println(money + " 원");
+            System.out.println(answer);
+            return answer;
+        } catch(IllegalArgumentException e) {
+            return Collections.emptyList(); // 빈 리스트 반환
+        }
     }
 
     public static void main (String[] args) {
-        // solution(withdrawMoney());
-        solution(50237);
+        // 1 ~ 1,000,000 자연수 무작위 생성
+        int money = (int)(Math.random() * 999999)+1;
+        solution(money);
     }
 }
