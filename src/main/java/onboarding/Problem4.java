d7aadea9b5136e9082cc2cecb8670c05577210d7
eeheg <sjh8924@naver.com>
2023-07-29 18:10:15 +0900

    refactor(Problem4): 문자열-아스키코드 상호변환 기능 개선 및 불필요한 동작 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,69 +2,50 @@ package onboarding;
 
 import java.util.*;
 
-/*
- * 아스키코드표
- *  A = 65 ~ Z = 90
- *  a = 97 ~ z = 122
- */
-
 public class Problem4 {
 
-    //메시지의 각 문자를 arrayList로 추출하는 메서드
-    static List<Character> getWord (String word) {
-        List<Character> words = new ArrayList<Character>();
-        char[] messageWords = word.toCharArray();
-
-        for (char c : messageWords) {
-            words.add(c);
-        }
-        return words;
-    }
-
-    //모든 문자를 각각 아스키코드로 변환하는 메서드
-    static List<Integer> getAscii(List<Character> words) {
-        List<Integer> ascii = new ArrayList<>();
-        for (char c : words) {
-            int asciicode = (byte)c;
-            ascii.add(asciicode);
-        }
-        return ascii;
-    }
-
-    //청개구리 사전의 아스키코드로 변환하는 메서드
-    static List<Integer> getCode(List<Integer> ascii) {
+    //문자열을 아스키코드 배열로 변환하는 메서드
+    static List<Integer> convertWordsToCodes (String words) {
         List<Integer> codes = new ArrayList<>();
-        for (int i : ascii) {
-            if (i==32) { //공백일 때
-                codes.add(i);
-            } else if (i<=90) { //대문자일 때
-                codes.add(155 - i);
-            } else if (i>=97) { //소문자일 때
-                codes.add(219 - i);
-            }
+        char[] messageWords = words.toCharArray();
+        for (char word : messageWords) {
+            codes.add((int)word);
         }
         return codes;
     }
 
-    //청개구리 사전의 아스키코드를 문자로 변환하는 메서드
-    static String getFrogMessage(List<Integer> codes) {
-        //char타입 arrayList에 (char)변환한 codes 삽입
-        List<Character> frogWords = new ArrayList<>();
-        for (int code : codes) {
-            char frogWord = (char)code;
-            frogWords.add(frogWord);
+    //아스키코드 배열을 문자열로 변환하는 메서드
+    static String convertCodesToWords (List<Integer> codes) {
+        StringBuilder words = new StringBuilder();
+        for(int code : codes) {
+            words.append((char)code);
         }
+        return words.toString();
+    }
 
-        //각 char를 String으로 변환하고 하나의 문자열을 생성
-        String FrogMessage = "";
-        for (char s : frogWords) {
-            FrogMessage += String.valueOf(s);
+    /*
+     * 아스키코드표
+     *  A = 65 ~ Z = 90
+     *  a = 97 ~ z = 122
+     */
+
+    //message의 아스키코드를 청개구리사전의 아스키코드로 변환하는 메서드
+    static List<Integer> convertToFrogCode(List<Integer> codes) {
+        List<Integer> frogCodes = new ArrayList<>();
+        for (int code : codes) {
+            if (code == 32) { //공백일 때
+                frogCodes.add(code);
+            } else if (code <= 90) { //대문자일 때
+                frogCodes.add(155 - code);
+            } else if (code >= 97) { //소문자일 때
+                frogCodes.add(219 - code);
+            }
         }
-        return FrogMessage;
+        return frogCodes;
     }
 
     public static String solution(String word) {
-        String answer = getFrogMessage(getCode(getAscii(getWord(word))));
+        String answer = convertCodesToWords(convertToFrogCode(convertWordsToCodes(word)));
         System.out.println(answer);
         return answer;
     }
